425114df5050faa4f042dbd99f9a36e4b339aea6
Vitor Rezende <42680847+vfrezende@users.noreply.github.com>
2020-10-05 10:14:45 -0300

    Commit #2

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main {
-    public static void main(String[] args) {
-      Tests tests = new Tests();
-      tests.testMultiplication();
-    }
+  public static void main(String[] args) {
+    Tests tests = new Tests();
+    tests.testMultiplication();
+    tests.testFrancMultiplication();
+    tests.testEquality();
   }
+} 
